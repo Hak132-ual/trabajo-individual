894328467b367ef341e32da45b34c0cf534e651a
Hak132-ual <159447477+Hak132-ual@users.noreply.github.com>
2024-03-04 16:03:56 +0000

    trabjo

---
+++ PR_2/templateholamundo.c
@@ -0,0 +1,34 @@
+/*
+** Descripción: Plantilla de ejecución repetida
+** Asunto: primeros pasos 
+*/
+
+
+#include <stdio.h> // operaciones de entrada salida
+#include <stdlib.h> // operaciones sobre memoria dinámica
+#include <ctype.h> // operaciones sobre cadenas
+#include <math.h>  //operaciones matemáticas
+
+
+int main()
+{
+	char c;
+
+	do
+	{
+		system("cls||clear"); // Limpia el terminal
+
+		printf("\n");
+		printf("======\n\n");
+
+#define NOMBRE "computadora"
+	{
+    printf("\n \n Hola mundo, me llamo  %s \n \n", NOMBRE);
+    return 0;
+    }
+
+		printf("\n\n¿Desea efectuar una nueva operación (s/n)? ");
+		scanf(" %c", &c);
+	} while ((c != 'N') && (c != 'n'));
+	return 0;
+}
