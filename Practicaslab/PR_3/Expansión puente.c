9266821ee1917750065ebb3cf3e1c9acf4124884
Hak132-ual <159447477+Hak132-ual@users.noreply.github.com>
2024-03-11 15:57:30 +0000

    trabajo clase

---
+++ Practicaslab/PR_3/Expansión puente.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+
+{
+
+   float const a = 11.7*pow(10. -6)
+   int Tf;
+   int Ti;
+   float incremento_longitud
+   int 1;
+   printf("cual es la temperatura final/n")
+   scanf("%d" , &Tf);
+    printf("cual es la temperatura inicial/n")
+    scanf("%d", &i);
+    printf("cuanto metros mides el puente/n")
+    scanf("%d" , &1);
+    incremento_longitud = a*1*(Tf-Ti);
+    scanf("esto es lo que aumenta la longitud: %.2f/n", incremento_longitud); 
+}
